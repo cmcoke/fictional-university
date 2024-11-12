a6c1f118cdd8be8d89d6649c46dd61c1f66f9ba8
cmcoke <minoru.cm@gmail.com>
2024-11-12 08:33:06 -0500

    Updated the front-page.html template for the 'fictional-block-theme' in order for the banner block to be viewed on the frontend.

--- themes/fictional-block-theme/our-blocks/archive.js
+++ themes/fictional-block-theme/our-blocks/archive.js
@@ -1,9 +1,9 @@
 wp.blocks.registerBlockType("ourblocktheme/archive", {
   title: "Fictional University Archive",
   edit: function () {
-    return wp.element.createElement("div", { className: "our-placeholder-block" }, "Archive Placeholder")
+    return wp.element.createElement("div", { className: "our-placeholder-block" }, "Archive Placeholder");
   },
   save: function () {
-    return null
+    return null;
   }
-})
+});
